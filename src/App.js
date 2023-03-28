5f5801be4128049e2a53687e3c1e74649d1f4aa4
shunhamm <shun.022885@gmail.com>
2023-03-28 16:30:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>SUSHIRO GACHA</h1>
+      </header>
+      <main>
+        <button className="gacha-button">ガチャを回す</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
